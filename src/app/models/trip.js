065f48ed2f004d824874f43c6cdd95ff6d604994
nkiru onwuneme <generalhrh@gmail.com>
2017-12-01 15:27:27 -0800

    Add trip model

---
+++ src/app/models/trip.js
@@ -0,0 +1,11 @@
+import Backbone from 'backbone';
+
+const Trip = Backbone.Model.extend({
+  urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
+  defaults: {
+    name: 'unknown'
+  }
+
+});
+
+export default Trip;
