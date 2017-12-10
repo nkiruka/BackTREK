92de10d0bc0de8bac2f6e7af1b58faeaff9f6ca8
nkiru onwuneme <generalhrh@gmail.com>
2017-12-10 01:37:40 -0800

    add validation for reservation and trip

--- src/app/models/reservation.js
+++ src/app/models/reservation.js
@@ -3,7 +3,31 @@ import Backbone from 'backbone';
 const Reservation = Backbone.Model.extend({
   url: function () {
     return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('trip_id') }/reservations`;
-  }
+  },
+
+  validate: function(attributes) {
+
+    const errors = {};
+    if (!attributes.name) {
+      errors['name'] = ["Name cannot be blank"];
+    }
+
+    if (!attributes.email) {
+      errors['email'] = ["Email cannot be blank"];
+    }
+
+    if (attributes.email) {
+      if (!attributes.email.includes('@')) {
+        errors['email'] = ["Enter valid email"];
+      }
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return errors;
+    }
+    return false;
+  },
+  
 });
 
 export default Reservation;
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -4,6 +4,38 @@ const Trip = Backbone.Model.extend({
   urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
   defaults: {
     name: 'unknown'
+  },
+  validate: function(attributes) {
+    const errors = {};
+
+    if (!attributes.name) {
+      errors['name'] = ["Name cannot be blank"];
+    }
+
+    if (!attributes.continent) {
+      errors['continent'] = ["Continent cannot be blank"];
+    }
+
+    if (!attributes.category) {
+      errors['category'] = ["Category cannot be blank"];
+    }
+
+    if (!attributes.weeks) {
+      errors['weeks'] = ["Weeks cannot be blank"];
+    }
+
+    if (!attributes.cost) {
+      errors['cost'] = ["Cost cannot be blank"];
+    }
+
+    if ( isNaN(attributes.weeks) ) {
+      errors['weeks'] = ["Weeks must be a number"];
+    }
+
+    if ( isNaN(attributes.cost) ) {
+      errors['cost'] = ["Cost must be a number"];
+    }
+
   }
 
 });
